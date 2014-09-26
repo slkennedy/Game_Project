e340c1b84ac33486b088d2446660b60b949d0de2
Jess Scheuring <jscheuring@gmail.com>
2014-09-26 11:35:48 -0400

    implements character constructor and tests

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
 var characters = [{name:'sara', health:100}];
+
+function Character(characterSelection) {
+  characterSelection = characterSelection || {};
+  this.name = (characterSelection.name) ? characterSelection.name : 'No Name';
+  this.health = (characterSelection.health) ? characterSelection.health : 100;
+}
--- test/spec/test.js
+++ test/spec/test.js
@@ -6,55 +6,69 @@
 describe('List Testing', function () {
 
 	it('should be an array', function () {
-		expect(characters).to.be.an('array')
-	})
-	
+		expect(characters).to.be.an('array');
+	});
+
 	it('should have objects for array items', function () {
-		expect(characters[0]).to.be.an('object')
-	})
-	
+		expect(characters[0]).to.be.an('object');
+	});
+
 	it('should have name and health properties for each object', function () {
-		expect(characters[0]).to.have.keys(['name','health'])
-  	
-	})
-})
+		expect(characters[0]).to.have.keys(['name','health']);
+
+	});
+});
 
 describe('Character Testing', function() {
 	describe('Instance Tests', function () {
-		it('should equal character', function() {
-			expect().to.be('')
-		})
+
+    beforeEach(function() {
+      this.goodGuy = new Character();
+    });
+
+    it('should exist', function() {
+
+			expect(this.goodGuy).to.be.ok;
+		});
+
+    it('should have the type "Character"', function() {
+
+      expect(this.goodGuy).to.be.an.instanceof(Character);
+
+    });
 
 		it('should have a name property', function() {
-			expect().to.be('')
-		})
+
+      expect(this.goodGuy.name).to.be.ok;
+		});
 
 		it('should have a health property', function () {
-			expect().to.be('')
-		})
+
+			expect(this.goodGuy.health).to.be.ok;
+		});
 
 		it('should have a type property', function () {
-			expect().to.be('')
-		})
-	})
+			expect().to.be('');
+		});
+	});
 
 	describe('Attack Function Testing', function (){
 		it('should have an attack function', function(){
-			expect().to.be('')
-		})
+			expect().to.be('');
+		});
 
 		it('should decrease the health level of the targe', function (){
-			expect().to.be('')
-		})
+			expect().to.be('');
+		});
 
 		it('should not let the health level go below 0', function (){
-			expect().to.be('')
-		})
+			expect().to.be('');
+		});
 
 		it('should be triggered to respond when health is 0', function(){
-			expect().to.be('')
-		})
-	})
-})
+			expect().to.be('');
+		});
+	});
+});
 
 })();

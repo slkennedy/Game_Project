ae86e93974408b5fe59f33cb57617189b5fe6a5d
slkennedy <slkennedy@gmail.com>
2014-09-26 11:47:45 -0400

    Adds dropdown template

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
 var characters = [{name:'sara', health:100}];
+
+function reusableTemplate(templateId, container, model) {
+    var templateFunction = _.template($('#' + templateId).text());
+    var renderedTemplate = templateFunction(model);
+    $(container).append(renderedTemplate);
+}
+
+_.each(characters, function(output){
+	reusableTemplate('characterList', '.dropdown', output);
+})

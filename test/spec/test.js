2260df627f521fb0e19b4f0e8a403d2cfd3c5948
slkennedy <slkennedy@gmail.com>
2014-09-26 00:22:10 -0400

    Adds tests with 3 passing

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,11 +3,58 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+describe('List Testing', function () {
 
-            });
-        });
-    });
+	it('should be an array', function () {
+		expect(characters).to.be.an('array')
+	})
+	
+	it('should have objects for array items', function () {
+		expect(characters[0]).to.be.an('object')
+	})
+	
+	it('should have name and health properties for each object', function () {
+		expect(characters[0]).to.have.keys(['name','health'])
+  	
+	})
+})
+
+describe('Character Testing', function() {
+	describe('Instance Tests', function () {
+		it('should equal character', function() {
+			expect().to.be('')
+		})
+
+		it('should have a name property', function() {
+			expect().to.be('')
+		})
+
+		it('should have a health property', function () {
+			expect().to.be('')
+		})
+
+		it('should have a type property', function () {
+			expect().to.be('')
+		})
+	})
+
+	describe('Attack Function Testing', function (){
+		it('should have an attack function', function(){
+			expect().to.be('')
+		})
+
+		it('should decrease the health level of the targe', function (){
+			expect().to.be('')
+		})
+
+		it('should not let the health level go below 0', function (){
+			expect().to.be('')
+		})
+
+		it('should be triggered to respond when health is 0', function(){
+			expect().to.be('')
+		})
+	})
+})
+
 })();

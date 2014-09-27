1b57698dfa14bf6a37b4dd4f3bc8c8503ad87f63
slkennedy <slkennedy@gmail.com>
2014-09-27 19:21:13 -0400

    Organizes JS code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,134 +1,132 @@
-var characters = [
-	{name:'sara', health:20, evil:false},
-	{name:'jess', health:20, evil:false},
-	{name:'ollie-berry', health:20, evil:true},
-	{name:'Jake-y-Poo', health:20, evil:true}
-];
+var characters = [{
+    name: 'sara',
+    health: 20,
+    evil: false
+}, {
+    name: 'jess',
+    health: 20,
+    evil: false
+}, {
+    name: 'ollie-berry',
+    health: 20,
+    evil: true
+}, {
+    name: 'Jake-y-Poo',
+    health: 20,
+    evil: true
+}];
 var goodCharacters = _.filter(characters, function(character) {
-  return character.evil === false;
+    return character.evil === false;
 });
 var badCharacters = _.filter(characters, function(character) {
-  return character.evil === true;
+    return character.evil === true;
 });
 var goodGuy;
 var badGuy;
 
-function reusableTemplate(templateId, container, model) {
-    var templateFunction = _.template($('#' + templateId).text());
-    var renderedTemplate = templateFunction(model);
-    $(container).append(renderedTemplate);
-}
-
-_.each(goodCharacters, function(output){
-	reusableTemplate('templates-character-list', '.dropdown', output);
+//user events
+//Reacts to form submission by creating a new instance of Character
+$("#pick-character").on('submit', function(event) {
+    event.preventDefault();
+    assignCharacters();
+    showGoodGuy();
+    stopShowingIt('#pick-character');
+    showBadGuy();
+    startShowingIt('.attack');
 });
 
+//Reacts to click of the attack button
+$('.attack').on('click', function(e) {
+    e.preventDefault();
+    goodGuy.attack(badGuy);
+    if (badGuy.health > 0) {
+        updateLifeStatus(badGuy);
+        badGuy.attack(goodGuy);
+        if (goodGuy.health > 0) {
+            updateLifeStatus(goodGuy);
+        } else {
+            gameover();
+        }
+    } else {
+        gameover();
+    }
+});
+
+//Character constructor
 function Character(characterSelection) {
-  characterModel = characterSelection || {};
-  this.name = (characterModel.name) ? characterModel.name : 'No Name';
-  this.health = (characterModel.health) ? characterModel.health : 100;
-  this.evil = (characterModel.evil) ? characterModel.evil : true;
+    characterModel = characterSelection || {};
+    this.name = (characterModel.name) ? characterModel.name : 'No Name';
+    this.health = (characterModel.health) ? characterModel.health : 100;
+    this.evil = (characterModel.evil) ? characterModel.evil : true;
 }
 
 //Attack Prototype
-Character.prototype.attack = function(attacked){
-	attacker = this.name;
-  attacked.health = attacked.health - _.random(1, 10);
+Character.prototype.attack = function(attacked) {
+    attacker = this.name;
+    attacked.health = attacked.health - _.random(1, 10);
 
 };
 
+//Update health display
 function updateLifeStatus(character) {
-    $('#health-number-'+character.name).empty();
-    $('#health-number-'+character.name).append(character.health);
+    $('#health-number-' + character.name).empty();
+    $('#health-number-' + character.name).append(character.health);
 }
-
-$('.attack').on('click', function(e) {
-  e.preventDefault();
-  goodGuy.attack(badGuy);
-  if (badGuy.health > 0) {
-    updateLifeStatus(badGuy);
-    badGuy.attack(goodGuy);
-      if (goodGuy.health > 0) {
-        updateLifeStatus(goodGuy);
-      }
-      else {
-        gameover();
-      }
-  }
-  else {
-    gameover();
-  }
-});
 
 //"Someone's being attacked", message
 function attackAlert(attacker, attacked) {
-  startShowingIt('.messages');
-  $('.messages').text(attacker+" is attacking "+attacked);
-  setTimeout(function() {
-      stopShowingIt('.messages');
-      }, 1000);
+    startShowingIt('.messages');
+    $('.messages').text(attacker + " is attacking " + attacked);
+    setTimeout(function() {
+        stopShowingIt('.messages');
+    }, 1000);
 }
-
 
 //create an extension of characters that makes a super evil badguy
 function SuperBadGuy(characterSelection) {
-  Character.apply(this, arguments);
-  this.health = 500;
+    Character.apply(this, arguments);
+    this.health = 500;
 }
 
 SuperBadGuy.prototype = Object.create(Character.prototype);
 
-//Reacts to form submission by creating a new instance of Character
-$("#pick-character").on('submit', function(event){
-	event.preventDefault();
-	assignCharacters();
-  showGoodGuy();
-  stopShowingIt('#pick-character');
-  showBadGuy();
-  startShowingIt('.attack');
-});
+
 
 //put the good guy into the dom
 function showGoodGuy() {
-  reusableTemplate('templates-display-character', '#good-guy', goodGuy);
+    reusableTemplate('templates-display-character', '#good-guy', goodGuy);
 }
 
+//put the bad guy into the dom
 function showBadGuy() {
-  reusableTemplate('templates-display-character', '#bad-guy', badGuy)
-}
-
-function stopShowingIt(what) {
-  $(what).addClass('hidden');
-}
-
-function startShowingIt(what) {
-  $(what).removeClass('hidden');
-}
-
-function assignCharacters(){
-	var playerSelection = $('.dropdown :selected').val();
-	var goodGuyModel = _.filter(characters, function(character){
-		return playerSelection === character.name;
-	});
-	goodGuyModel = goodGuyModel[0];
-	goodGuy = new Character(goodGuyModel);
-
-  var badGuyModel = _.sample(badCharacters) //random badguy;
-  var badGuyType = _.random(1);
-  if (badGuyType === 0) {
-      badGuy = new SuperBadGuy(badGuyModel);
-  }
-  else {
-	    badGuy = new Character(badGuyModel);
-  }
+    reusableTemplate('templates-display-character', '#bad-guy', badGuy)
 }
 
 
+//taking user choice of charcter and auto-generating bad guy
+function assignCharacters() {
+    var playerSelection = $('.dropdown :selected').val();
+    var goodGuyModel = _.filter(characters, function(character) {
+        return playerSelection === character.name;
+    });
+    goodGuyModel = goodGuyModel[0];
+    goodGuy = new Character(goodGuyModel);
 
+    var badGuyModel = _.sample(badCharacters) //random badguy;
+    var badGuyType = _.random(1);
+    if (badGuyType === 0) {
+        badGuy = new SuperBadGuy(badGuyModel);
+    } else {
+        badGuy = new Character(badGuyModel);
+    }
+}
+
+
+//gameover message
 function gameover(name) {
-  stopShowingIt('.show-character');
-  startShowingIt('.messages');
-  $('.messages').text('game-over');
+    stopShowingIt('.show-character');
+    startShowingIt('.messages');
+    $('.messages').text('game-over');
 }
 
 //
@@ -208,6 +206,23 @@ function gameover(name) {
 //   });
 // });
 
+//general functions
+function reusableTemplate(templateId, container, model) {
+    var templateFunction = _.template($('#' + templateId).text());
+    var renderedTemplate = templateFunction(model);
+    $(container).append(renderedTemplate);
+}
 
+_.each(goodCharacters, function(output) {
+    reusableTemplate('templates-character-list', '.dropdown', output);
+});
+
+function stopShowingIt(what) {
+    $(what).addClass('hidden');
+}
+
+function startShowingIt(what) {
+    $(what).removeClass('hidden');
+}
 
 //

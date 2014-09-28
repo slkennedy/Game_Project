eeef3fce4816371e278139610ce6fa9f61b9f630
Jess Scheuring <jscheuring@gmail.com>
2014-09-28 00:55:09 -0400

    makes page-load.js

---
+++ app/scripts/page-load.js
@@ -0,0 +1,19 @@
+// ## what happens on page load?
+// - user is prompted how many players? (user can pick a number)
+// - after user picks a number the gameplay function is initiated
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
